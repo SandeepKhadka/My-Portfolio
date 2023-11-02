761de90454086676eaf892a563a3242f11eaf91e
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2023-11-02 13:51:36 +0545

    Making quote responsive

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,4 +1,5 @@
-import { Col, Row, Alert } from "react-bootstrap";
+import { Col, Row } from "react-bootstrap";
+import "./Quotes.css";
 
 export const Quotes = () => {
   const containerStyle = {
@@ -9,36 +10,36 @@ export const Quotes = () => {
   };
 
   const quoteStyle = {
-    fontSize: "50px",
+    // fontSize: "50px",
     textAlign: "center",
     backgroundImage:
       "-webkit-linear-gradient(50deg, #f00, #f90, #ff0, #0f0, #00f, #90f, #f00)",
     WebkitBackgroundClip: "text",
     WebkitTextFillColor: "transparent",
   };
-  
 
   const authorStyle = {
     fontStyle: "italic",
     textAlign: "right",
-    fontSize: "16px",
+    // fontSize: "8px",
     color: "black",
-    backgroundImage:
-      "-webkit-linear-gradient(#f06, #f90, #f60",
+    backgroundImage: "-webkit-linear-gradient(#f06, #f90, #f60",
     WebkitBackgroundClip: "text",
     WebkitTextFillColor: "transparent",
   };
 
   return (
-    <Col lg={12}>
-      <div className="newsletter-bx wow slideInUp" style={containerStyle}>
-        <div>
-          <p style={quoteStyle}>
-            "Java is to JavaScript what car is to carpet"
-          </p>
-          <p style={authorStyle}>- Chris Heilmann</p>
+    <Row>
+      <Col lg={12} md={12} sm={12}>
+        <div className="newsletter-bx wow slideInUp" style={containerStyle}>
+          <div>
+            <p className="quote">
+              "Java is to JavaScript what car is to carpet"
+            </p>
+            <p style={authorStyle}>- Chris Heilmann</p>
+          </div>
         </div>
-      </div>
-    </Col>
+      </Col>
+    </Row>
   );
 };
