4c268f3f1a68a2d04425283bd7ff69a25f70bd3b
SandeepKhadka <87227387+SandeepKhadka@users.noreply.github.com>
2023-11-02 21:20:57 +0545

    Completed Updating Projects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,9 +16,9 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/sandeep-khadka-289637224/" target="/blank"><img src={navIcon1} alt="Icon" /></a>
+              <a href="https://www.facebook.com/sandeep.khadka.75436" target="/blank"><img src={navIcon2} alt="Icon" /></a>
+              <a href="https://www.instagram.com/san_deepkhadka/" target="/blank"><img src={navIcon3} alt="Icon" /></a>
             </div>
             <p className="footerText">Copyright 2023. All Rights Reserved</p>
           </Col>
